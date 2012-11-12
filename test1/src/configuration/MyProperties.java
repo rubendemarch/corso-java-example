6893301a9b03c2192aaf44381f879f5f417236a2
gabriele.bertaina@gmail.com <gabriele.bertaina@gmail.com@3bb91b27-d7c1-2427-62b2-0e1e5cddbddf>
2012-11-12 13:50:57 +0000

    aggiunto warpper per properties

--- test1/src/configuration/MyProperties.java
+++ test1/src/configuration/MyProperties.java
@@ -6,9 +6,13 @@ package configuration;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.InvalidPropertiesFormatException;
 import java.util.Properties;
 
 import util.MyLogger;
+import exception.ConfigFileNotFound;
+import exception.ConfigFileNotValid;
 
 /**
  * @author Dr
@@ -32,24 +36,44 @@ public class MyProperties {
 		logger.end(metodo);
 	}
 
-	public String get(String key) throws Exception{
-		final String metodo="get";
+	public String getPropertyValue(String key) throws ConfigFileNotFound {
+		final String metodo="getPropertyValue";
 		if(inputStream==null){
 			try {
 				inputStream=new FileInputStream(pathFile);
 			} catch (FileNotFoundException e) {
 				logger.fatal(metodo, "tentativo connessione file di properties", e);
-				throw new Exception("file di configurazione non trovato");
+				throw new ConfigFileNotFound("file di configurazione non trovato");
 			}
 		}
 		if(properties==null){
 			properties=new Properties();
 			if(pathFile.toUpperCase().endsWith(".XML")){
-				properties.loadFromXML(inputStream);
+				try {
+					properties.loadFromXML(inputStream);
+				} catch (InvalidPropertiesFormatException e) {
+					logger.fatal(metodo, "file corrotto", e);
+					throw new ConfigFileNotValid( "file corrotto");
+				} catch (IOException e) {
+					logger.fatal(metodo, "impossibile leggere il file di properties.xml", e);
+					throw new ConfigFileNotValid( "impossibile leggere il file di properties.xml");
+				}
 			}else if(pathFile.toUpperCase().endsWith(".PROPERTIES")){
-				properties.load(inputStream);
+				try {
+					properties.load(inputStream);
+				} catch (IOException e) {
+					logger.fatal(metodo, "impossibile leggere il file di properties.properties", e);
+					throw new ConfigFileNotValid( "impossibile leggere il file di properties.properties");
+				}
+			}
+			try {
+				inputStream.close();
+			} catch (IOException e) {
+				logger.warn(metodo, "fallito tentativo chiusura configInputStream", e);
 			}
 		}
 		return properties.getProperty(key);
 	}
+
+	//public 
 }
