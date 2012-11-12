c30deb18d001f99d1e1cade5917180cd9ad4567c
gabriele.bertaina@gmail.com <gabriele.bertaina@gmail.com@3bb91b27-d7c1-2427-62b2-0e1e5cddbddf>
2012-11-12 14:25:33 +0000

    aggiunto warpper per properties

---
+++ test1/src/exception/config/ConfigFileCreateEx.java
@@ -0,0 +1,30 @@
+package exception.config;
+
+public class ConfigFileCreateEx extends Config {
+
+	public ConfigFileCreateEx() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConfigFileCreateEx(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConfigFileCreateEx(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConfigFileCreateEx(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConfigFileCreateEx(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ test1/src/exception/config/ConfigFileNotFound.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package exception.config;
+
+/**
+ * @author Dr
+ *
+ */
+public class ConfigFileNotFound extends Config {
+
+	/**
+	 * 
+	 */
+	public ConfigFileNotFound() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public ConfigFileNotFound(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public ConfigFileNotFound(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public ConfigFileNotFound(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 * @param enableSuppression
+	 * @param writableStackTrace
+	 */
+	public ConfigFileNotFound(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
