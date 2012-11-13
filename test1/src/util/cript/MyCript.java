51195ce555fd7eb57ae28ba5dad856da1ecdc098
gabriele.bertaina@gmail.com <gabriele.bertaina@gmail.com@3bb91b27-d7c1-2427-62b2-0e1e5cddbddf>
2012-11-13 16:28:22 +0000

    config cript

---
+++ test1/src/util/cript/MyCript.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package util.cript;
+
+import org.apache.commons.lang3.StringUtils;
+
+
+
+/**
+ * @author Dr
+ * 
+ */
+public class MyCript {
+
+	/**
+	 * 
+	 */
+	public MyCript() {
+	}
+
+	public static String encrypt(String input) {
+		StringBuilder ret = new StringBuilder();
+		for (char c :input.toCharArray()){
+			ret.append(StringUtils.leftPad(((int)c)+"", 3, '0'));
+		}
+		return ret.toString();
+	}
+
+	public static String decrypt(String input) {
+		int start=0;
+		int end = 3;
+		int cntCicli=input.length()/3;
+		StringBuilder ret= new StringBuilder();
+		for(int i=0;i<cntCicli;i++){
+			if(i!=cntCicli-1){
+			ret.append((char)Integer.parseInt(input.substring(start, end)));
+			}else{
+				ret.append((char)Integer.parseInt(input.substring(start)));
+			}
+			start+=3;
+			end+=3;
+		}
+		return ret.toString();
+	}
+
+}
