c9ac2f81ff0137510bc57921012b21dc3fc8fa0b
gabriele.bertaina@gmail.com <gabriele.bertaina@gmail.com@3bb91b27-d7c1-2427-62b2-0e1e5cddbddf>
2012-12-18 10:38:23 +0000

    validazioni

--- trunk/thECommerce/WebContent/js/common/form.js
+++ trunk/thECommerce/WebContent/js/common/form.js
@@ -10,3 +10,15 @@ function loadAll(commonFormActionValue,commonActionValue,commonIdValue){
 	document.getElementById('commonForm').action=commonFormActionValue;
 	document.getElementById('commonForm').submit();
 }
+function validate(evt){
+	var theEvent = evt || window.event;
+	var key = theEvent.keyCode || theEvent.which;
+	key = String.fromCharCode(key);
+	var regex = /[0-9]|[a-z]|[A-Z]|\ |\?/;
+	if (!regex.test(key)) {
+		theEvent.returnValue = false;
+		if (theEvent.preventDefault){
+			theEvent.preventDefault();
+		}
+	}
+}
