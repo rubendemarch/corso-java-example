df88cf1310cbb9a42380e8f3a85289d0f29b50f2
gabriele.bertaina@gmail.com <gabriele.bertaina@gmail.com@3bb91b27-d7c1-2427-62b2-0e1e5cddbddf>
2013-01-08 11:28:20 +0000

    db login

--- trunk/LoginStruts2/src/struts2/LoginAction.java
+++ trunk/LoginStruts2/src/struts2/LoginAction.java
@@ -3,7 +3,17 @@
  */
 package struts2;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -14,9 +24,31 @@ import com.opensymphony.xwork2.ActionSupport;
 public class LoginAction extends ActionSupport {
 	private static final long serialVersionUID = -6206808590907715484L;
 
+	private SqlSessionFactory sqlSessionFactory=null;
+
 	private String userName;
 	private String password;
 	private Date myBirthday;
+
+	private Pattern pattern;
+
+	public LoginAction() {
+		String resource = "mybatis-config.xml";
+		InputStream inputStream=null;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if(inputStream!=null){
+			try {
+				sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		pattern = Pattern.compile( "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{8,20})");
+	}
 
 	public Date getMyBirthday() {
 		return myBirthday;
@@ -42,8 +74,15 @@ public class LoginAction extends ActionSupport {
 		this.password = password;
 	}
 
+	public String getMd5Password(){
+		return DigestUtils.md5Hex(password);
+	}
+
 	public String execute() {
-		if (this.userName.equals("admin") && this.password.equals("admin123")) {
+		SqlSession sql = sqlSessionFactory.openSession();
+		int ok = sql.selectOne("User.login",this);
+		sql.close();
+		if (ok>0) {
 			return SUCCESS;
 		} else {
 			addActionError(getText("error.login"));
@@ -56,14 +95,19 @@ public class LoginAction extends ActionSupport {
 			addFieldError("userName",
 								getText("requiredstring",
 											new String[]{getText("userName")}));
-		} else if (!getUserName().equals("Eswar")) {
-			addFieldError("userName", getText("error.user"));
 		}
 		if (getPassword().length() == 0) {
 			addFieldError("password",
 								getText("requiredstring",
 											new String[]{getText("password")}));
+		}else if (getPassword().length()<8) {
+			addFieldError("password",
+								getText("error.length",
+										new String[]{getText("password")}));
+		}else if(!pattern.matcher(password).matches()){
+			addFieldError("password",
+					getText("error.format",
+							new String[]{getText("password")}));
 		}
 	}
-
 }
